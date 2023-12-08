7598b129c672f78d98b8aa60989c77ebf46b8d12
Firmino07 <153281029+Firmino07@users.noreply.github.com>
2023-12-08 08:33:10 -0300

    Add files via upload

---
+++ Inicio.js
@@ -0,0 +1,38 @@
+
+// Função para abrir o pop-up
+function abrirPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+}
+
+
+// Função para fechar o pop-up
+function fecharPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
+/* Abrir popup apos 5 segundos */
+setTimeout(function () {
+    abrirPopup();
+}, 4000);
+
+
+/* Fechar Popup apos 10 segundos */
+setTimeout(function () {
+    fecharPopup();
+}, 15000);
+
+
+
+
+
+
+// Função para fechar o pop-up
+function fecharPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
+
+
+
+
